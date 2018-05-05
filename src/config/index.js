5edde2f097c7fa71eda2debdc2a12bdc3e25db30
cjh <1418503647@qq.com>
2018-05-05 14:52:16 +0800

    update API domain

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ let imgDomain
 let apiDomain
 if (process.env.NODE_ENV === 'production') {
     imgDomain = 'http://120.79.29.47'
-    apiDomain = 'http://node.api.yunser.com'
+    apiDomain = 'https://nodeapi.yunser.com'
 } else {
     imgDomain = 'http://120.79.29.47'
     apiDomain = 'http://localhost:1026'
