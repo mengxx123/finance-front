452c9198e691605a53cd42f30dc50af6d0021956
cjh <1418503647@qq.com>
2018-05-27 16:13:44 +0800

    优化

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,8 @@ if (process.env.NODE_ENV === 'production') {
     apiDomain = 'https://nodeapi.yunser.com'
 } else {
     imgDomain = 'http://120.79.29.47'
-    apiDomain = 'http://localhost:1026'
+    // apiDomain = 'http://localhost:1026'
+    apiDomain = 'https://nodeapi.yunser.com'
 }
 
 module.exports = {
